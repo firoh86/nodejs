96abc9cc2f35fbeb8116d2b3b580fd2d5cde63a4
firoh86 <firohh.as@gmail.com>
2020-02-26 00:10:00 +0100

    hola mundo

---
+++ hola-mundo.js
@@ -0,0 +1,8 @@
+console.log("Hola mundo");
+
+/*
+ Cuando creamos una aplicación con nodejs esta estará dividida en varias partes,
+cada una de estas partes será llamada módulo.
+Y a su vez, cada uno de éstos módulos tendrá asignada una tarea.
+
+*/
